3d61171950d220d1ca28b2cf4c4ab39a6be63dc9
Sourav Das <94233240+Dassourav07@users.noreply.github.com>
2022-06-26 12:59:54 +0530

    Initial Commit

---
+++ Frontend/src/components/TextContainer/TextContainer.js
@@ -0,0 +1,36 @@
+import React from 'react';
+
+import onlineIcon from '../../icons/onlineIcon.png';
+
+import './TextContainer.css';
+
+const TextContainer = ({ users }) => (
+  <div className="textContainer">
+    <div>
+      <h1>Welcome to Live Text<span role="img" aria-label="emoji">💬</span></h1>
+      <h2>Dear friend, welcome!  <span role="img" aria-label="emoji">👋</span></h2>
+      <h2> Nothing was the same without you during your absence!<span role="img" aria-label="emoji">😍 </span></h2>
+    </div>
+    {
+      users
+        ? (
+          <div>
+            <h1>People currently chatting:</h1>
+            <div className="activeContainer">
+              <h2>
+                {users.map(({name}) => (
+                  <div key={name} className="activeItem">
+                    {name}
+                    <img alt="Online Icon" src={onlineIcon}/>
+                  </div>
+                ))}
+              </h2>
+            </div>
+          </div>
+        )
+        : null
+    }
+  </div>
+);
+
+export default TextContainer;
