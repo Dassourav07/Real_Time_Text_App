82371a381e7d059e3773a7c937caf3cf70ebf5cb
Sourav Das <94233240+Dassourav07@users.noreply.github.com>
2022-08-31 15:57:25 +0530

    Delete Backend directory

--- Backend/router.js
+++
@@ -1,8 +0,0 @@
-const express = require("express")
-const router = express.Router();
-router.get('/', (req, res)=> {
-res.send({ response :"The server is running sucessfully!!"}).status(2000);
-});
-
-
-module.exports = router;
